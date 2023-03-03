0ad86fe89ddb23d0f0179516aa1734f24676b2e1
RiahiIP <ayanleyetosin@gmail.com>
2023-03-03 17:13:33 -0500

    Develop front-end

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,10 +1,16 @@
-// import logo from './logo.svg';
 import './App.css';
+
+import Document from '../../components/Document/Document'
+import PatentSearch from '../../components/PatentSearch/PatentSearch'
 
 export default function App() {
   return (
-    <main className="App">
-      <h1>React Web Scraper</h1>
+    <main className="App bg-secondary">
+      <h1 className='text-center'>React Web Scraper</h1>
+      <div className='row'>
+        <PatentSearch />
+        <Document />
+      </div>
     </main>
   );
 }
